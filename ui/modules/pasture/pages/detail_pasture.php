af39ea8c7f6c283e24811d2c57fca1d25422ae23
Elton Santana <elton.santana@elton.santana>
2023-08-31 14:22:19 -0300

    Começanod pasto

---
+++ ui/modules/pasture/pages/detail_pasture.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Visualizar Pasto</title>
+    <link rel="stylesheet" type="text/css" href="../styles/create_style.css">
+    <script src="../scripts/create_script.js"></script>
+</head>
+<body>
+<div class="form-container">
+        <h2>Editar Fazenda</h2>
+        <?php
+        if (isset($_GET['pastureId']) && is_numeric($_GET['pastureId'])) {
+            $selectedPastureId = $_GET['pastureId'];
+
+            require_once 'C:/xampp/htdocs/ProjetoFinalWeb2/ui/business/farm_business.php';
+            $pastureBusiness = new PastureBusiness();
+
+            try {
+                $selectedFarm = $pastureBusiness->searchFarm($selectedFarmId, null)->getFarms()[0];
+
+                if ($selectedFarm) {
+                    $imagePath = str_replace('C:/xampp/htdocs/ProjetoFinalWeb2/', '../../../../', $selectedFarm->farmImage);
+                    echo '<form action="../process/update_process_farm.php" method="post" enctype="multipart/form-data">';
+                    echo '<input type="hidden" name="farmId" value="' . $selectedFarmId . '">';
+                    echo '<input type="hidden" name="oldFarmImage" value="' . $selectedFarm->farmImage . '">';
+                    echo '<label for="farmImage">Imagem da Fazenda:</label>';
+                    echo '<input type="file" name="farmImage" id="farmImage" accept="image/*" onchange="updateImagePreview(event)">';
+                    echo '<div class="image-preview">';
+                    echo '<img id="imagePreview" src="' . $imagePath . '" alt="Preview da Imagem">';
+                    echo '</div>';
+                    echo '<label for="farmName">Nome da Fazenda:</label>';
+                    echo '<input type="text" id="farmName" name="farmName" value="' . $selectedFarm->farmName . '" required>';
+                    echo '<label for="farmDescription">Descrição da Fazenda:</label>';
+                    echo '<textarea id="farmDescription" name="farmDescription" rows="4" required>' . $selectedFarm->farmDescription . '</textarea>';
+                    echo '<div class="button-container">';
+                    echo '<button type="button" onclick="goBack()">Voltar</button>';
+                    echo '<button type="submit">Salvar Edições</button>';
+                    echo '</div>';
+                    echo '</form>';
+                    echo '<div id="message-dialog" style="display: none;">';
+                    echo '<p id="message-text"></p>';
+                    echo '<button id="close-button">Fechar</button>';
+                    echo '</div>';
+
+                } else {
+                    echo 'Fazenda não encontrada.';
+                }
+            } catch (\Throwable $th) {
+                echo 'Erro ao buscar fazenda: ' . $th->getMessage();
+            }
+        } else {
+            echo 'ID da fazenda não fornecido ou inválido.';
+        }
+        ?>
+    </div>
+</body>
+</html>
